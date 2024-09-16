4fb83813d1d136e35ae88b4124fd07cb8eb6c591
MobyScript <Mobarkbacran1088@gmail.com>
2024-09-16 15:41:07 +0300

    Update cursor chaser to use animated Casper images

--- contentScript.js
+++ contentScript.js
@@ -1,13 +1,55 @@
 const follower = document.createElement("div");
 follower.id = "Casper";
 document.body.appendChild(follower);
+const images = {
+  left: [
+    chrome.runtime.getURL("images/left1.png"),
+    chrome.runtime.getURL("images/left2.png"),
+  ],
+  right: [
+    chrome.runtime.getURL("images/right1.png"),
+    chrome.runtime.getURL("images/right2.png"),
+  ],
+  upLeft: [
+    chrome.runtime.getURL("images/upleft1.png"),
+    chrome.runtime.getURL("images/upleft2.png"),
+  ],
+  upRight: [
+    chrome.runtime.getURL("images/upright1.png"),
+    chrome.runtime.getURL("images/upright2.png"),
+  ],
+  downLeft: [
+    chrome.runtime.getURL("images/downleft1.png"),
+    chrome.runtime.getURL("images/downleft2.png"),
+  ],
+  downRight: [
+    chrome.runtime.getURL("images/downright1.png"),
+    chrome.runtime.getURL("images/downright2.png"),
+  ],
+  up: [
+    chrome.runtime.getURL("images/up1.png"),
+    chrome.runtime.getURL("images/up2.png"),
+  ],
+  down: [
+    chrome.runtime.getURL("images/down1.png"),
+    chrome.runtime.getURL("images/down2.png"),
+  ],
+};
+
+// Preload the images
+Object.keys(images).forEach((direction) => {
+  images[direction].forEach((src) => {
+    const img = new Image();
+    img.src = chrome.runtime.getURL(src);
+  });
+});
 
 const gifImage = document.createElement("img");
-gifImage.src = chrome.runtime.getURL("casper.gif");
-gifImage.alt = "Casper";
-
+gifImage.src = chrome.runtime.getURL("images/left1.png");
+gifImage.alt = "Casper!";
 follower.appendChild(gifImage);
 
+// Random starting position
 function getRandomPosition() {
   const width = window.innerWidth;
   const height = window.innerHeight;
@@ -20,6 +62,7 @@ let { x: followerX, y: followerY } = getRandomPosition();
 follower.style.top = `${followerY}px`;
 follower.style.left = `${followerX}px`;
 
+// Mouse position tracking
 let mouseX = 0;
 let mouseY = 0;
 document.addEventListener("mousemove", (e) => {
@@ -27,8 +70,40 @@ document.addEventListener("mousemove", (e) => {
   mouseY = e.pageY;
 });
 
+// Determine direction of movement
+function getDirection(followerX, followerY, mouseX, mouseY) {
+  const dx = mouseX - followerX;
+  const dy = mouseY - followerY;
+  const angle = Math.atan2(dy, dx) * (180 / Math.PI);
+
+  if (angle >= -135 && angle <= -45) {
+    return "up";
+  } else if (angle >= 45 && angle <= 135) {
+    return "down";
+  } else if (angle > -45 && angle < 45) {
+    return "right";
+  } else if (angle > 135 || angle < -135) {
+    return "left";
+  } else if (angle > -135 && angle < -90) {
+    return "upLeft";
+  } else if (angle > -90 && angle < -45) {
+    return "upRight";
+  } else if (angle > 90 && angle < 135) {
+    return "downLeft";
+  } else if (angle > 45 && angle < 90) {
+    return "downRight";
+  }
+}
+
+let frame = 4;
+function updateImage(follower, direction) {
+  const imageSrc = images[direction][frame % 2];
+  follower.querySelector("img").src = imageSrc;
+  frame++;
+}
+
 function moveFollower() {
-  const speed = 0.5;
+  const speed = 2;
   const dx = mouseX - followerX;
   const dy = mouseY - followerY;
 
@@ -38,7 +113,10 @@ function moveFollower() {
   follower.style.top = `${followerY}px`;
   follower.style.left = `${followerX}px`;
 
-  requestAnimationFrame(moveFollower);
+  const direction = getDirection(followerX, followerY, mouseX, mouseY);
+  updateImage(follower, direction);
 }
+
+setInterval(moveFollower, 200);
 
 moveFollower();
